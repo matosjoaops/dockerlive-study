d297501f99c3505baf423e75f017eb8c6a630e38
João Matos <matosjoaops@protonmail.com>
2023-04-28 16:07:27 +0100

    Added if to check if var is set

--- grab_dockerfiles.sh
+++ grab_dockerfiles.sh
@@ -2,6 +2,12 @@
 
 # The only argument is the participant's name (without spaces)
 
-cp ~/Desktop/task-1/Dockerfile ~/STUDY_FILES/Dockerfile.$1.task-1
-cp ~/Desktop/task-2/Dockerfile ~/STUDY_FILES/Dockerfile.$1.task-2
-cp ~/Desktop/task-3/Dockerfile ~/STUDY_FILES/Dockerfile.$1.task-3
+if [[ -n "$1" ]];
+then
+  echo "Grabbing Dockerfiles from participant $1..."
+  cp ~/Desktop/task-1/Dockerfile ~/STUDY_FILES/Dockerfile.$1.task-1
+  cp ~/Desktop/task-2/Dockerfile ~/STUDY_FILES/Dockerfile.$1.task-2
+  cp ~/Desktop/task-3/Dockerfile ~/STUDY_FILES/Dockerfile.$1.task-3
+else
+  echo "Error: participant name not provided!"
+fi
