f40bfba7850e82c5f8895df688293ed36defa3aa
matosjoaops <matosjoaops@protonmail.com>
2023-04-28 11:52:25 +0100

    Added more scripts to automate study

---
+++ grab_dockerfiles.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# The only argument is the participant's name (without spaces)
+
+cp ~/Desktop/task-1/Dockerfile ~/STUDY_FILES/Dockerfile.$1.task-1
+cp ~/Desktop/task-2/Dockerfile ~/STUDY_FILES/Dockerfile.$1.task-2
+cp ~/Desktop/task-3/Dockerfile ~/STUDY_FILES/Dockerfile.$1.task-3
