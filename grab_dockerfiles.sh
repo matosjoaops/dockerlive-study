3f93f6ff1c810284c07c2ba0f186edf7c4176a7a
matosjoaops <matosjoaops@protonmail.com>
2023-04-30 12:31:19 +0100

    Updated directory name in script

--- grab_dockerfiles.sh
+++ grab_dockerfiles.sh
@@ -5,9 +5,9 @@
 if [[ -n "$1" ]];
 then
   echo "Grabbing Dockerfiles from participant $1..."
-  cp ~/Desktop/task-1/Dockerfile ~/STUDY_FILES/Dockerfile.$1.task-1
-  cp ~/Desktop/task-2/Dockerfile ~/STUDY_FILES/Dockerfile.$1.task-2
-  cp ~/Desktop/task-3/Dockerfile ~/STUDY_FILES/Dockerfile.$1.task-3
+  cp ~/Desktop/task-1/Dockerfile ~/.STUDY_FILES/Dockerfile.$1.task-1
+  cp ~/Desktop/task-2/Dockerfile ~/.STUDY_FILES/Dockerfile.$1.task-2
+  cp ~/Desktop/task-3/Dockerfile ~/.STUDY_FILES/Dockerfile.$1.task-3
 else
   echo "Error: participant name not provided!"
 fi
