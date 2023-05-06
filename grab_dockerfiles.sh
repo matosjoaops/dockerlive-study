51edf4eec8617fd37eb6e09633a043382844b589
matosjoaops <matosjoaops@protonmail.com>
2023-05-06 16:35:12 +0100

    Updated dockerfile script to copy tmp files just in case

--- grab_dockerfiles.sh
+++ grab_dockerfiles.sh
@@ -7,6 +7,9 @@ if [[ -n "$1" ]]; then
   cp ~/Desktop/task-1/Dockerfile ~/.STUDY_FILES/Dockerfile.$1.task-1
   cp ~/Desktop/task-2/Dockerfile ~/.STUDY_FILES/Dockerfile.$1.task-2
   cp ~/Desktop/task-3/Dockerfile ~/.STUDY_FILES/Dockerfile.$1.task-3
+  cp ~/Desktop/task-1/tmp.Dockerfile ~/.STUDY_FILES/tmp.Dockerfile.$1.task-1
+  cp ~/Desktop/task-2/tmp.Dockerfile ~/.STUDY_FILES/tmp.Dockerfile.$1.task-2
+  cp ~/Desktop/task-3/tmp.Dockerfile ~/.STUDY_FILES/tmp.Dockerfile.$1.task-3
 else
   echo "Error: participant email not provided!"
 fi
